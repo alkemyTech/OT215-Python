536fd29fe15dd27d559b3d6913d649a7097f92a6
Lautaro Romero <lautaro.r89@gmail.com>
2022-05-25 01:16:20 -0300

    Creación de directorio de trabajo y consultas para grupo de universidades F

---
+++ lautaro/consulta_UMoron.sql
@@ -0,0 +1,3 @@
+SELECT universidad AS university, carrerra AS career, TO_DATE(fechaiscripccion, 'DD/MM/YYYY') AS inscription_date, SPLIT_PART(nombrre, ' ', 1) AS first_name, SPLIT_PART(nombrre, ' ', 2) AS last_name, sexo AS gender, (CURRENT_DATE - TO_DATE(nacimiento, 'DD/MM/YYYY')) / 365 AS age, codgoposstal AS postal_code, eemail AS email
+FROM moron_nacional_pampa
+WHERE fechaiscripccion >= '2020-09-01' AND fechaiscripccion >= '2021-02-01' AND universidad = 'Universidad de morón'
