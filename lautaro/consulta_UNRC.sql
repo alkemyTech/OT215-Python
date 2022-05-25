90d5b9d82543adfe91563d2a591bbbbfbd12ff27
Lautaro Romero <lautaro.r89@gmail.com>
2022-05-25 01:33:01 -0300

    Creación de directorio de trabajo y consultas SQL para grupo de universidades F

--- lautaro/consulta_UNRC.sql
+++ lautaro/consulta_UNRC.sql
@@ -1,3 +1,3 @@
 SELECT univiersities AS university, carrera AS career, TO_DATE(inscription_dates, 'YY/MON/DD') AS inscription_date, SPLIT_PART(names, '-', 1) AS first_name, SPLIT_PART(names, '-', 2) AS last_name, sexo AS gender, (CURRENT_DATE - TO_DATE(fechas_nacimiento, 'YY/MON/DD')) / 365 AS age, localidad AS localidad, email
 FROM rio_cuarto_interamericana
-WHERE inscription_dates >= '2020-09-01' AND inscription_dates >= '2021-02-01' AND univiersities = 'Universidad-nacional-de-río-cuarto'
+WHERE TO_DATE(inscription_dates, 'YY/MON/DD') >= '2020-09-01' AND TO_DATE(inscription_dates, 'YY/MON/DD') <= '2021-02-01' AND univiersities = 'Universidad-nacional-de-río-cuarto'
