9ee22967bc8c3db03ee479cbfcfe1657aa135364
Sebastian Piratova <sebasps94@gmail.com>
2022-06-01 13:37:58 -0500

    OT215-44

--- sebastian/dag_logs.py
+++ sebastian/dag_logs.py
@@ -1,19 +1,27 @@
+"""
+OT215-44 Sprint 2 A
+Configurar logs para Universidad De Flores y Universidad Nacional De Villa María
+utilizando la libreria Loggin de python (%Y-%m-%d - nombre_logger - mensaje).
+"""
+
 from datetime import timedelta, datetime
 import logging.config
 
 from airflow import DAG
 
-logging.config.fileConfig('logging.conf')
+# Logger configuration.
+logging.config.fileConfig(f'airflow/dags/scripts/logging.conf')
 logger = logging.getLogger('DAG')
 logger.info("Starting logs.")
 
-default_args = {
-    'retries': 5,
+# Retries configuration.
+DEFAULT_ARGS = {
+    "retries": 5,
 }
 
 with DAG(
-    default_args=default_args,
-    dag_id='analysis_uflo_unvm',
+    default_args=DEFAULT_ARGS,
+    dag_id='logs_configuration',
     description="""
          Data extraction with postgresql for UFLO and UNVM universities, 
          data processing with pandas, 
