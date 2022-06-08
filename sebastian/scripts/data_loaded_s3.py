ad0f98dc5b59c7a7346270e83d31f3d787da022d
Sebastian Piratova <sebasps94@gmail.com>
2022-06-08 18:26:25 -0500

    OT215-84 y 85

--- sebastian/scripts/data_loaded_s3.py
+++ sebastian/scripts/data_loaded_s3.py
@@ -1,6 +1,13 @@
+import logging.config
+
 from airflow.hooks.S3_hook import S3Hook
 
 
+# Data upload to s3.
 def connection_to_s3(filename, key, bucket_name):
+	logger = logging.getLogger("DAG")
+	logger.info("Establishing connection to the cloud.")
 	hook = S3Hook("aws_s3")
+	logger.info("Successful connection.")
 	hook.load_file(filename=filename, key=key, bucket_name=bucket_name)
+	logger.info(f"{key} upload successfully.")
